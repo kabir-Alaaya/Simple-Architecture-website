ade5bd54feaeff2c74c476a1adbd7ff59e8a30a3
kaybee001 <kabiralaaya@gmail.com>
2024-08-09 11:44:22 +0100

    Add hamburger menu to make it responsive on small devices

---
+++ script.js
@@ -0,0 +1,9 @@
+// Nav hamburgerburger selections
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+  });
